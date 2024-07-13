33f16955bc663d6a8d20c95e13e1807a0d4f0366
Artemas-Muzanenhamo <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2024-07-13 20:06:37 +0100

    add LightSaber solution

---
+++ src/main/kyu8/LightSabers.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace csharpcodewars.src.main.kyu8
+{
+    public static class LightSabers
+    {
+        public static int HowManyLightsabersDoYouOwn(string name) => "Zach".Equals(name) ? 18 : 0;
+    }
+}
---
+++ src/test/kyu8/LightSabersTest.cs
@@ -0,0 +1,19 @@
+using System;
+using csharpcodewars.src.main.kyu8;
+using NUnit.Framework;
+
+namespace csharpcodewars.src.test.kyu8
+{
+    [TestFixture]
+    public class LightSabersTest
+    {
+        [Test]
+        public static void Adam() => Assert.That(LightSabers.HowManyLightsabersDoYouOwn("Adam"), Is.EqualTo(0));
+
+        [Test]
+        public static void Zach() => Assert.That(LightSabers.HowManyLightsabersDoYouOwn("Zach"), Is.EqualTo(18));
+
+        [Test]
+        public static void NullName() => Assert.That(LightSabers.HowManyLightsabersDoYouOwn(null), Is.EqualTo(0));
+    }
+}
