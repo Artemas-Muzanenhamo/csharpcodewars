9cabfe9c2598322a438bd90db27c289652c46080
Artemas-Muzanenhamo <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2024-07-13 22:46:59 +0100

    add namespace to test

--- src/test/kyu8/SuperDuperEasyTest.cs
+++ src/test/kyu8/SuperDuperEasyTest.cs
@@ -2,19 +2,24 @@ using System;
 using csharpcodewars.src.main.kyu8;
 using NUnit.Framework;
 
-[TestFixture]
-public class SuperDuperEasyTest {
+namespace csharpcodewars.src.test.kyu8
+{
+    [TestFixture]
+    public class SuperDuperEasyTest
+    {
 
-    [Test]
-    public void Test1() {
-        Assert.Multiple(() =>
+        [Test]
+        public void Test1()
         {
-            Assert.That(SuperDuperEasy.Problem("hello"), Is.EqualTo("Error"));
-            Assert.That(SuperDuperEasy.Problem("1"), Is.EqualTo("56"));
-            Assert.That(SuperDuperEasy.Problem("5"), Is.EqualTo("256"));
-            Assert.That(SuperDuperEasy.Problem("0"), Is.EqualTo("6"));
-            Assert.That(SuperDuperEasy.Problem("1.2"), Is.EqualTo("66"));
-        });
+            Assert.Multiple(() =>
+            {
+                Assert.That(SuperDuperEasy.Problem("hello"), Is.EqualTo("Error"));
+                Assert.That(SuperDuperEasy.Problem("1"), Is.EqualTo("56"));
+                Assert.That(SuperDuperEasy.Problem("5"), Is.EqualTo("256"));
+                Assert.That(SuperDuperEasy.Problem("0"), Is.EqualTo("6"));
+                Assert.That(SuperDuperEasy.Problem("1.2"), Is.EqualTo("66"));
+            });
 
+        }
     }
 }
